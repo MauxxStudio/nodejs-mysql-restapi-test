574346bb4cffeb5f96cf400ea46417f85d9b7da5
Mauxx <mchamorro.ing@gmail.com>
2023-03-23 20:23:13 -0300

    first commit

---
+++ db/database.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS Companydb;
+
+USE Companydb;
+
+CREATE TABLE employee (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) DEFAULT NULL,
+    salary INT(5) DEFAULT NULL,
+    PRIMARY KEY (ID)
+);
+
+DESCRIBE employee;
+
+INSERT INTO employee VALUES
+(1,'Joe',1000),
+(2,'Henry',2000);
